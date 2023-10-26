2ac46f21aeb46c63aa4ff2cee1c5e212d028558d
1228ma <1962423582@qq.com>
2023-10-26 09:40:19 +0800

    projecthome

---
+++ utils/tools.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/registry/consul"
+)
+
+func GetMicroClient() client.Client {
+	consulReg := consul.NewRegistry()
+	microService := micro.NewService(
+		micro.Registry(consulReg),
+	)
+	return microService.Client()
+}
